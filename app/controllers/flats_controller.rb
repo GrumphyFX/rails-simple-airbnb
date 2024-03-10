e37974b9532e52e96282bed291b7baf673aed5ab
Bart Przespolewski <150512708+GrumphyFX@users.noreply.github.com>
2024-03-10 11:51:06 +0100

    rails-simple-airbnb

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -44,6 +44,6 @@
         private
       
         def flat_params
-          params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+            params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture_url)
         end
     end
